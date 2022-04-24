36aa81e34747f080644942b21a2dce355f09a4ca
Kailash Meghwar <premanikailash97@gmail.com>
2022-04-24 22:28:56 +0500

    Sweeping

---
+++ Swapping.php
@@ -0,0 +1,15 @@
+<?php 
+     
+echo " Two veriable swapping assignment .<br><br>";
+
+$number1= 10;
+$number2= 20;
+
+$number1= $number1+$number2;   
+$number2=$number1-$number2;   
+$number1=$number1-$number2;  
+
+echo " Number1 value is = ".$number1;
+echo"<br>";
+echo " Number2 value is = ".$number2;
+?>
